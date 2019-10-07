60239f06863acf3d7a81af2ed95a54cfd915b90a
Pavel-Sake <sake211091@gmail.com>
2019-10-07 23:22:15 +0300

    feature: add solution for 'js-edu' function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+  focus = 'family',
+  knowsProgramming = true,
+  config = {family: 4}
+) {
+  const hoursForCourse = 800;
+  const hoursForBasicCourse = 500;
+
+  if (knowsProgramming) {
+    const weeks = Math.ceil(hoursForCourse / config[focus]);
+    return weeks
+  }
+  const weeks = Math.ceil((hoursForCourse + hoursForBasicCourse) / config[focus]);
+  return weeks
+};
   
